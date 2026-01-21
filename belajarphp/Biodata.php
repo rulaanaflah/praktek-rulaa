01c9083de01324fd12d75d1136298a0a3fcde452
rulaanaflah <rulaanaflah@gmail.com>
2026-01-21 09:36:38 +0700

    update dan ganti

---
+++ belajarphp/Biodata.php
@@ -0,0 +1,23 @@
+<?php
+    $Nama = "Rulaa Nur Naflah";
+    $Kelas = "XI TJKT 1";
+    $Jurusan = "Teknik Jaringan Komputer Telekomunikasi";
+    $Alamat = "Kp. Parung Halang Rt/Rw 05/01 No.09 Kel.Andir Kec.Baleendah Kab.Bandung"
+    $Hobi = "Main Basket";
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Biodata</title>
+</head>
+<body>
+
+<h2>Biodata Siswa</h2>
+Nama : <?php echo  $Nama = "Rulaa Nur Naflah"; ?><Br>
+Kelas : <?php echo  $Kelas = "XI TJKT 1"; ?><Br>
+Jurusan : <?php echo $Jurusan = "Teknik Jaringan Komputer Telekomunikasi"; ?><Br>
+Alamat : <?php echo  $Alamat = "Kp. Parung Halang Rt/Rw 05/01 No.09 Kel.Andir Kec.Baleendah Kab.Bandung"; ?><Br>
+Hobi : <?php echo $Hobi = "Main Basket"; ?><Br>
+
+</body>
+</html>
