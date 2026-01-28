2fa15552ea3c9a71b4f567d97adb3d560041812c
rulaanaflah <rulaanaflah@gmail.com>
2026-01-28 09:04:36 +0700

    penugasan

---
+++ belajarphp/Penugasan.php
@@ -0,0 +1,10 @@
+<?php
+    $speed = 83;
+    echo "speed awal: $speed <Br>";
+
+    $speed = $speed + 10;
+    echo "setelah di tambah 10 (aritmatika): $speed <Br>";
+
+    $speed += 10;
+    echo "setelah di tambah 10 (penugasan): $speed <Br>";
+?>
