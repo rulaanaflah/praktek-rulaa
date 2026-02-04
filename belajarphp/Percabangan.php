7374ddb30c371562bd3ae4172e28693da94e7929
rulaanaflah <rulaanaflah@gmail.com>
2026-02-04 08:05:31 +0700

    percabangan

---
+++ belajarphp/Percabangan.php
@@ -0,0 +1,26 @@
+<?php
+// Percabangan If
+    $total_belanja = 150000;
+
+if($total_belanja > 100000){
+    echo "Anda dapat hadiah!";
+}
+
+// Percabangan If/Else
+    $umur = 13;
+
+    if ($umur < 18 ){
+        echo "Kamu tidak boleh membuka situs ini!";
+    } else {
+        echo "Selamat datang di website kami!";
+    }
+    $umur = 19;
+
+    if ($umur < 18 ){
+        echo "Kamu tidak boleh membuka situs ini!";
+    } else {
+        echo "Selamat datang di website kami!";
+    }
+
+    
+?>
