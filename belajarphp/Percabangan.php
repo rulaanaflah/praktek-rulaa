d4a6ca87408f6ccc10ef8d51f685308d2f54b52c
rulaanaflah <rulaanaflah@gmail.com>
2026-02-04 08:20:02 +0700

    ping

--- belajarphp/Percabangan.php
+++ belajarphp/Percabangan.php
@@ -21,4 +21,30 @@ if($total_belanja > 100000){
     } else {
         echo "<p>Selamat datang di website kami!</P>";
     }
+
+ // Percabangan If/Elseif/Else
+    $nilai = 88;
+
+    if ($nilai > 90) {
+        $grade = "A+";
+    } elseif($nilai > 80){
+        $grade = "A";
+    } elseif($nilai > 70){
+        $grade = "B+";
+    } elseif($nilai > 60){
+        $grade = "B";
+    } elseif($nilai > 50){
+        $grade = "C+";
+    } elseif($nilai > 40){
+        $grade = "C";
+    } elseif($nilai > 30){
+        $grade = "D";
+    } elseif($nilai > 20){
+        $grade = "E";
+    } else {
+        $grade = "F";
+    }
+
+    echo "Nilai anda: $nilai<br>";
+    echo "Grade: $grade";
 ?>
