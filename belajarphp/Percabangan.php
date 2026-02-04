2c7b6f3e1cce06cf4ad1d0d435415f5f1b8a718e
rulaanaflah <rulaanaflah@gmail.com>
2026-02-04 08:22:08 +0700

    test

--- belajarphp/Percabangan.php
+++ belajarphp/Percabangan.php
@@ -47,4 +47,24 @@ if($total_belanja > 100000){
 
     echo "Nilai anda: $nilai<br>";
     echo "Grade: $grade";
+
+// Percabangan Switch/Case
+    $level = 3;
+
+switch($level){
+    case 1:
+        echo "Pelajari HTML";
+        break;
+    case 2:
+        echo "Pelajari CSS";
+        break;
+    case 3:
+        echo "Pelajari Javascript";
+        break;
+    case 4:
+        echo "Pelajari PHP";
+        break;
+    default:
+        echo "Kamu bukan programmer!";
+}
 ?>
