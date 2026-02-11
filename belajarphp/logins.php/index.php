af0195422e621c9a7e6b0ffae465c47f6a503cea
rulaanaflah <rulaanaflah@gmail.com>
2026-02-11 09:28:38 +0700

    ping

--- belajarphp/logins.php/index.php
+++ belajarphp/logins.php/index.php
@@ -6,21 +6,21 @@
 <body>
     <form action="login.php" method="POST">
         <fieldset>
-            <legend>Login</legend>
-            <p>
-                <label>Username:</label>
-                <input type="text" name="username" placehoder="usename..." />
-            </p>
-            <p>
-                <label>Password:</label>
-                <input type="text" name="password" placeholder="password..." />
-            </p>
-            <p>
-                <label><input> type="checkbox" name="remember" value="remeber" /> Remember me</label>
-            </p>
-            <p>
-                <input> type="submit" name="submit" value="Login" />
-            </p>
+        <legend>Login</legend>
+        <p>
+            <label>Username:</label>
+            <input type="text" name="username" placeholder="username..." />
+        </p>
+        <p>
+            <label>Password:</label>
+            <input type="password" name="password" placeholder="password..." />
+        </p>
+        <p>
+            <label><input type="checkbox" name="remember" value="remember" /> Remember me</label>
+        </p>
+        <p>
+            <input type="submit" name="submit" value="Login" />
+        </p>
         </fieldset>
     </form>
 </body>
