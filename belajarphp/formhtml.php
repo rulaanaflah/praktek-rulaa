c4214edcd4966af436d6627fa3a0ed703c854149
rulaanaflah <rulaanaflah@gmail.com>
2026-02-11 08:31:59 +0700

    coba

---
+++ belajarphp/formhtml.php
@@ -0,0 +1,10 @@
+<h1>Form Html Dengan PHP</h1>
+    <form action="proses.php" method="post">
+        <div>
+            <Label>Nama</label><input type="text" name= "nama"/>
+        </div>
+        <div>
+            <Label>Email</label><input type="text" name="email"/>
+        </div>
+        <input type="submit" value="Simpan"/>
+</form>
---
+++ belajarphp/login.php
@@ -0,0 +1,4 @@
+<?php
+    $nama = $_POST[`nama`];
+    echo "halo," . $nama . "!Data Kamu Sudah Di Terima.";
+?>
